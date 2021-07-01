22318a00c23e35947f55aee0dd3eee413d4a102e
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-01 18:06:03 -0400

    make form controlled form, add state

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function Form(props) {
+  const [location, setLocation] = useState(null);
+  function handleChange(e) {
+    setLocation(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.getWeather(location);
+  }
   return (
     <div className="d-flex justify-content-center">
-      <form className="w-50">
+      <form className="w-50" onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Choose a Destination
@@ -12,6 +23,7 @@ export default function Form(props) {
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Choose a City"
+            onChange={handleChange}
           />
         </div>
         <button type="submit" className="btn btn-primary">
