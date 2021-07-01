d10f2e7da0a6a2db6a25615433965788ed621932
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-01 18:34:56 -0400

    add test get route for server

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,10 @@
 import Form from "./Form";
+import axios from "axios";
 
 export default function Home(props) {
   function getWeather(location) {
     console.log("From home---", location);
+    axios.get("/weatherData").then((data) => console.log(data.data));
   }
   return (
     <div>
