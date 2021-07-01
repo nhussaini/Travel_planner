48dacdd225c208152630e29e539cbd563e09bf20
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-01 18:02:25 -0400

    add a simple input form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,3 +1,23 @@
 export default function Form(props) {
-  return <div>Coming from Form</div>;
+  return (
+    <div className="d-flex justify-content-center">
+      <form className="w-50">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Choose a Destination
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Choose a City"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
