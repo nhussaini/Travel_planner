a991152d551c9199ff4424c0ef35a469b3e5fab4
tandica <williams.tandica@gmail.com>
2021-07-02 15:06:44 -0400

    add register form

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,8 +1,37 @@
 export default function Register(props){
 
   return (
-    <div>
-      Register
+    <div className="d-flex justify-content-center box">
+      <form>
+        <div className="mb-3 text-left">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3 text-left">
+          <label for="exampleInputPassword1" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   )
 }
