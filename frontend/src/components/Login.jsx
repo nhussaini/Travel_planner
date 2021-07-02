044f3b6fc5776be3c2cbca722609b27ab5cb6379
tandica <williams.tandica@gmail.com>
2021-07-02 16:46:18 -0400

    implement login page styling

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,36 +1,45 @@
+import "../styles/Login.scss";
+
 export default function Login(props) {
   return (
-    <div className="d-flex justify-content-center box">
-      <form>
-        <div className="mb-3 text-left">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+    <body class="login-bg">
+      <div class="top-div-login">
+        <div class="login-box">
+          <div class="register-title">Register</div>
+          <div class="input-forms">
+            <form>
+              <div className="mb-3 text-left">
+                <label for="exampleInputEmail1" class="label-name">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  class="input-form"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Email..."
+                />
+              </div>
+              <div class="mb-3 text-left">
+                <label for="exampleInputPassword1" class="label-name">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password..."
+                />
+              </div>
+
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
-        <div class="mb-3 text-left">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+      </div>
+    </body>
   );
 }
