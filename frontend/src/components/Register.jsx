517cdca3631ff71c199d3c5e5c64ae9576354772
tandica <williams.tandica@gmail.com>
2021-07-02 15:30:20 -0400

    add preliminary styling for register form

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,46 +1,51 @@
-export default function Register(props){
+import "../styles/Register.scss";
 
+export default function Register(props) {
   return (
-    <div className="d-flex justify-content-center box">
-      <div>Register</div>
-      <form>
-      <div className="mb-3 text-left">
-          <label for="exampleInputEmail1" className="form-label">
-            Full Name
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
+    <div>
+      <div class="register-title">Register</div>
+      <div class="input-forms">
+        <form>
+          <div>
+            <label for="exampleInputName" className="form-label">
+              Full Name
+            </label>
+            <input
+              type="email"
+              class="input-form"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
           </div>
-        <div className="mb-3 text-left">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div class="mb-3 text-left">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
+          <div className="mb-3 text-left">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="input-form"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div class="mb-3 text-left">
+            <label for="exampleInputPassword1" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
