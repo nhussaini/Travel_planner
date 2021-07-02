2b1d506650010b95162b3e1bf183c7d29d76a565
tandica <williams.tandica@gmail.com>
2021-07-02 15:11:33 -0400

    update register form to include full name

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,7 +2,19 @@ export default function Register(props){
 
   return (
     <div className="d-flex justify-content-center box">
+      <div>Register</div>
       <form>
+      <div className="mb-3 text-left">
+          <label for="exampleInputEmail1" className="form-label">
+            Full Name
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          </div>
         <div className="mb-3 text-left">
           <label for="exampleInputEmail1" className="form-label">
             Email address
@@ -13,9 +25,6 @@ export default function Register(props){
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
         <div class="mb-3 text-left">
           <label for="exampleInputPassword1" class="form-label">
