1ae3d1e39c514c9afd0dae3ebe2c459dc5f201c1
tandica <williams.tandica@gmail.com>
2021-07-04 15:27:19 -0400

    test different info

--- frontend/src/components/CityDetails.jsx
+++ frontend/src/components/CityDetails.jsx
@@ -1,11 +1,11 @@
 
 export default function CityDetails (props){
-
+console.log("props test", props.locationData.attributes)
 return (
 <div>
     City Details
     <p>Where: 
-        <span>{props.locationData.attributes.long_name}</span>
+        {/* <span>{props.locationData.attributes.population}</span> */}
     </p>
 </div>
 )
