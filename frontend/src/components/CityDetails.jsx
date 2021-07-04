1ccb1ff4fa353ba3fee9c409dc70033225b8c900
tandica <williams.tandica@gmail.com>
2021-07-04 14:42:53 -0400

    display name of city on page

--- frontend/src/components/CityDetails.jsx
+++ frontend/src/components/CityDetails.jsx
@@ -1,15 +1,11 @@
 
 export default function CityDetails (props){
-    //console.log("propss", props.locationData.attributes.long_name)
 
-    if (props.locationData) {
-        console.log("test props", props.locationData.attributes)
-    } 
 return (
 <div>
     City Details
     <p>Where: 
-        {/* <span>{props.locationData.attributes.population}</span> */}
+        <span>{props.locationData.attributes.long_name}</span>
     </p>
 </div>
 )
