5fcc3ff41d9e87cca6b479232c19a622f493ebc0
tandica <williams.tandica@gmail.com>
2021-07-04 14:36:16 -0400

    display dummy data and try to render info on page

--- frontend/src/components/CityDetails.jsx
+++ frontend/src/components/CityDetails.jsx
@@ -1,8 +1,16 @@
 
 export default function CityDetails (props){
+    //console.log("propss", props.locationData.attributes.long_name)
+
+    if (props.locationData) {
+        console.log("test props", props.locationData.attributes)
+    } 
 return (
 <div>
     City Details
+    <p>Where: 
+        {/* <span>{props.locationData.attributes.population}</span> */}
+    </p>
 </div>
 )
 }
