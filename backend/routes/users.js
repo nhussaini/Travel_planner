e1f0bbc867a7d382d647bb8f42fd3d349d2b91f4
tandica <williams.tandica@gmail.com>
2021-07-05 21:18:29 -0400

    insert new users into db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,7 +9,14 @@ module.exports = (db) => {
 
 
   router.post('/', (req, res, next) => {
-    db.query(`SELECT * FROM users`).then((data) => console.log("query data", data))
+    const { firstName, lastName, email, password } = req.body
+    const queryString = `INSERT INTO users (first_name, last_name, email, password)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id;
+  `
+
+    //db.query(`SELECT * FROM users`).then((data) => console.log("query data", data))
+    db.query(queryString, [firstName, lastName, email, password]).then((data) => console.log("query data", data))
     //console.log("db", db)
     res.send('post routeee');
   });
