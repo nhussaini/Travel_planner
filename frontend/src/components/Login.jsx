bc6a7b25663a637d6ba5069660041522e9aebe0a
tandica <williams.tandica@gmail.com>
2021-07-06 16:56:21 -0400

    update styling for login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -28,41 +28,43 @@ export default function Login(props) {
   }
 
   return (
-    <div className="top-div-login login-bg">
-      <div className="login-box">
-        <div className="login-title">Login</div>
-        <div className="input-forms">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3 text-left">
-              <label for="exampleInputEmail1" class="label-name">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="input-form form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Email..."
-                onChange={handleEmailChange}
-              />
-            </div>
-            <div className="mb-3 text-left">
-              <label for="exampleInputPassword1" class="label-name">
-                Password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password..."
-                onChange={handlePasswordChange}
-              />
-            </div>
+    <div className="login-bg">
+      <div className="top-div-login">
+        <div className="login-box">
+          <div className="login-title">Login</div>
+          <div className="input-forms">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3 text-left">
+                <label for="exampleInputEmail1" class="label-name">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="input-form form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Email..."
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="mb-3 text-left">
+                <label for="exampleInputPassword1" class="label-name">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password..."
+                  onChange={handlePasswordChange}
+                />
+              </div>
 
-            <button type="submit" className="btn btn-dark">
-              Submit
-            </button>
-          </form>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
