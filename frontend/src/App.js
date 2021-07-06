381ecbc79d805ec15dab2e546f384bb396bb2bd2
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-06 18:58:50 -0400

    add new component for location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import "./styles/app.scss";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import LocationDetail from "./components/LocationDetail";
 
 function App() {
   // const [token, setToken] = useState();
@@ -25,6 +26,9 @@ function App() {
           </Route>
           <Route path="/register">
             <Register />
+          </Route>
+          <Route path="/cities/:id">
+            <LocationDetail />
           </Route>
         </Switch>
       </Router>
