c3aad463a931352b2d7bac2ce58e3c61993e7f1e
tandica <williams.tandica@gmail.com>
2021-07-06 17:07:16 -0400

    update styling for register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -37,67 +37,69 @@ export default function Register(props) {
   }
 
   return (
-    <div className="top-div">
-      <div className="register-box">
-        <div className="register-title">Register</div>
-        <div className="input-forms">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="exampleInputName" className="label-name">
-                First Name
-              </label>
-              <input
-                type="text"
-                className="input-form form-control"
-                id="exampleInputFirstName"
-                aria-describedby="nameHelp"
-                placeholder="Name..."
-                onChange={handleFirstNameChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputLastName" className="label-name">
-                Last Name
-              </label>
-              <input
-                type="text"
-                className="input-form form-control"
-                id="exampleInputLastName"
-                aria-describedby="nameHelp"
-                placeholder="Name..."
-                onChange={handleLastNameChange}
-              />
-            </div>
-            <div className="mb-3 text-left">
-              <label htmlFor="exampleInputEmail" className="label-name">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="input-form form-control"
-                id="exampleInputEmail"
-                aria-describedby="emailHelp"
-                placeholder="Email..."
-                onChange={handleEmailChange}
-              />
-            </div>
-            <div className="mb-3 text-left">
-              <label htmlFor="exampleInputPassword" className="label-name">
-                Password
-              </label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword"
-                placeholder="Password..."
-                onChange={handlePasswordChange}
-              />
-            </div>
+    <div className="register-bg">
+      <div className="top-div">
+        <div className="register-box">
+          <div className="register-title">Register</div>
+          <div className="input-forms-register">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputName" className="label-name">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  className="input-form-register form-control"
+                  id="exampleInputFirstName"
+                  aria-describedby="nameHelp"
+                  placeholder="Name..."
+                  onChange={handleFirstNameChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputLastName" className="label-name">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  className="input-form-register form-control"
+                  id="exampleInputLastName"
+                  aria-describedby="nameHelp"
+                  placeholder="Name..."
+                  onChange={handleLastNameChange}
+                />
+              </div>
+              <div className="mb-3 text-left">
+                <label htmlFor="exampleInputEmail" className="label-name">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="input-form-register form-control"
+                  id="exampleInputEmail"
+                  aria-describedby="emailHelp"
+                  placeholder="Email..."
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="mb-3 text-left">
+                <label htmlFor="exampleInputPassword" className="label-name">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword"
+                  placeholder="Password..."
+                  onChange={handlePasswordChange}
+                />
+              </div>
 
-            <button type="submit" className="btn btn-dark">
-              Submit
-            </button>
-          </form>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
