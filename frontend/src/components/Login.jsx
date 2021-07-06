b70e34c4d5fe749a7942ecaa7db16473c55691ce
tandica <williams.tandica@gmail.com>
2021-07-06 13:18:38 -0400

    create user auth for login and clean up code for register page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,45 +1,63 @@
+import { useState } from "react";
+import axios from "axios";
 import "../styles/Login.scss";
 
 export default function Login(props) {
-  return (
-    <body class="login-bg">
-      <div class="top-div-login">
-        <div class="login-box">
-          <div class="login-title">Login</div>
-          <div class="input-forms">
-            <form>
-              <div className="mb-3 text-left">
-                <label for="exampleInputEmail1" class="label-name">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  class="input-form"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Email..."
-                />
-              </div>
-              <div class="mb-3 text-left">
-                <label for="exampleInputPassword1" class="label-name">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password..."
-                />
-              </div>
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-              <button type="submit" className="btn btn-dark">
-                Submit
-              </button>
-            </form>
-          </div>
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("/users", { email: email, password: password })
+      .then((data) => console.log(data));
+  }
+  return (
+    <div className="top-div-login login-bg">
+      <div className="login-box">
+        <div className="login-title">Login</div>
+        <div className="input-forms">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3 text-left">
+              <label for="exampleInputEmail1" class="label-name">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="input-form form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Email..."
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="mb-3 text-left">
+              <label for="exampleInputPassword1" class="label-name">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password..."
+                onChange={handlePasswordChange}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-dark">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
