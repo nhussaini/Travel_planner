739efcca967a51f1accd37ffa46d5c224cb7251c
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-06 13:28:29 -0400

    show markers for top things to do on the map

--- frontend/src/components/GoogleMap.jsx
+++ frontend/src/components/GoogleMap.jsx
@@ -46,9 +46,10 @@
 //   apiKey: "AIzaSyAKiSBRO8K9Y_DwPGTadsCGzSh7p589d-A",
 // })(GoogleMap);
 
+import { useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 
-export default function GoogleMap() {
+export default function GoogleMap(props) {
   const loader = new Loader({
     apiKey: "AIzaSyAKiSBRO8K9Y_DwPGTadsCGzSh7p589d-A",
     version: "weekly",
@@ -57,21 +58,42 @@ export default function GoogleMap() {
 
   const mapOptions = {
     center: {
-      lat: 43.6532,
-      lng: -79.3832,
+      lat: props.lat,
+      lng: props.long,
     },
     zoom: 12,
   };
-
+  // const map;
   // Promise
-  loader
-    .load()
-    .then((google) => {
-      new google.maps.Map(document.getElementById("map"), mapOptions);
-    })
-    .catch((e) => {
-      // do something
-      console.log("Something is Wrong");
-    });
+  console.log(props.thingsToDo[0].geometry.location);
+  useEffect(() => {
+    loader
+      .load()
+      .then((google) => {
+        let map = new google.maps.Map(
+          document.getElementById("map"),
+          mapOptions
+        );
+
+        props.thingsToDo.map((thing) => {
+          return new google.maps.Marker({
+            position: thing.geometry.location,
+            map,
+            title: "Hello World!",
+          });
+        });
+
+        // new google.maps.Marker({
+        //   position: { lat: -25.363, lng: 131.044 },
+        //   map,
+        //   title: "Hello World!",
+        // });
+      })
+      .catch((e) => {
+        // do something
+        console.log("Something is Wrong---", e);
+      });
+  });
+
   return <div id="map"></div>;
 }
