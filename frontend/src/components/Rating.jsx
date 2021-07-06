5f270938162ca469a4c89090179bea0ae895eef8
unknown <nasrullah_hussaini@yahoo.com>
2021-07-06 13:28:54 -0700

    add a custom fucntion for rating

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,6 +1,15 @@
 import ReactStars from "react-rating-stars-component";
 export default function Rating(props) {
   // const rating = <ReactStars count={5} size={24} activeColor="#ffd700" />;
+  function roundRating(rating) {
+    if (rating > 4.5) {
+      return Math.ceil(rating);
+    } else if (rating < 4.5) {
+      return Math.floor(rating);
+    } else {
+      return rating;
+    }
+  }
 
   return (
     <div className="rating">
@@ -8,7 +17,7 @@ export default function Rating(props) {
         count={5}
         size={24}
         activeColor="#ffd700"
-        value={props.rating}
+        value={roundRating(props.rating)}
         isHalf
         edit={false}
       />
