941ceca3c2ed68993dc67fc3f7701d791d1c9d07
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-07 02:23:59 -0400

    get data from roadtgoat api, send all data together rom backend to frontend

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -4,11 +4,41 @@ const axios = require("axios");
 require("dotenv").config();
 
 module.exports = (db) => {
+  //roadGhoat Credentials
+  const roadGoatApiAuth = {
+    auth: {
+      username: "bc8bd73ed4416f3824623910174c4bcf",
+      password: "d7055907b51a5ece2b5e4c715fadd789",
+    },
+  };
+
   router.post("/getCityData", (req, res) => {
+    const cityName = req.body.userInput;
     const allData = {};
-    const imageCall = `https://api.unsplash.com/search/photos?page=1&query=${req.body.userInput}&client_id=${process.env.imageKEY}&per_page=8`;
+    // since Roaggoat data needs two api call, getting this data first before making calls to other api.
+    // first get the id of the location for further api calls
+
+    // saving all api quesries in variables
+    const imageCall = `https://api.unsplash.com/search/photos?page=1&query=${cityName}&client_id=${process.env.imageKEY}&per_page=8`;
     // const weatherCall = `https://api.weatherbit.io/v2.0/forecast/daily?city=${req.body.userInput}&key=${process.env.weatherKEY}&days=7`;
-    const googleCall = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+${req.body.userInput}&key=AIzaSyD6Gw9uN4YpFcH4cIjRbYbWKPl_vGQs0R0`;
+    const googleCall = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+${cityName}&key=AIzaSyD6Gw9uN4YpFcH4cIjRbYbWKPl_vGQs0R0`;
+    axios
+      .get(
+        `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${cityName}`,
+        roadGoatApiAuth
+      )
+      .then((data) => {
+        const locationId = data.data.data[0].id;
+        // api call to get location data using the location id from previous call
+        axios
+          .get(
+            `https://api.roadgoat.com/api/v2/destinations/${locationId}`,
+            roadGoatApiAuth
+          )
+          .then((data) => {
+            allData.cityData = data.data.data;
+          });
+      });
 
     Promise.all([
       axios.get(imageCall),
