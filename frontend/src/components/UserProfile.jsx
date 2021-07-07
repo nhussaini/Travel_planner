d8ca220fe4840623bd09cf0cacd49af9433cb605
tandica <williams.tandica@gmail.com>
2021-07-07 18:09:24 -0400

    add styling to user profile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -6,41 +6,51 @@ export default function UserProfile(props) {
       <h2>My Trips</h2>
       <div className="user-trip-card card">
         <div className="city-name">Paris</div>
-
-        <div>
-          <img
-            className="user-trip-img"
-            alt="city"
-            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-          />
-
-          <div className="user-things">
-            <img
-              className="user-trip-img"
-              alt="city"
-              src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-            />
-
-            <img
-              className="user-trip-img"
-              alt="city"
-              src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-            />
-
-            <img
-              className="user-trip-img"
-              alt="city"
-              src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-            />
-
-            <img
-              className="user-trip-img"
-              alt="city"
-              src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-            />
-            {/* <p>{allTrendingCities}</p> */}
-          </div>
-        </div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
+      </div>
+      <div className="user-trip-card card">
+        <div className="city-name">Paris</div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
+      </div>
+      <div className="user-trip-card card">
+        <div className="city-name">Paris</div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
+      </div>
+      <div className="user-trip-card card">
+        <div className="city-name">Paris</div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
+      </div>
+      <div className="user-trip-card card">
+        <div className="city-name">Paris</div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
+      </div>
+      <div className="user-trip-card card">
+        <div className="city-name">Paris</div>
+        <img
+          className="user-trip-img"
+          alt="city"
+          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+        />
       </div>
     </div>
   );
