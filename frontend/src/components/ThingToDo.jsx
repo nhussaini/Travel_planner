6eb7b5493e4838af7533a1b6d4c4b13c657498ed
unknown <nasrullah_hussaini@yahoo.com>
2021-07-06 21:33:34 -0700

    add favorite icon

--- frontend/src/components/ThingToDo.jsx
+++ frontend/src/components/ThingToDo.jsx
@@ -1,4 +1,6 @@
 import Rating from "./Rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function ThingToDo(props) {
   return (
@@ -9,12 +11,19 @@ export default function ThingToDo(props) {
            &photoreference=${props.reference}&key=AIzaSyD6Gw9uN4YpFcH4cIjRbYbWKPl_vGQs0R0`}
         alt={props.name}
       />
+      <div>
+        <FontAwesomeIcon icon={faHeart} size="lg" />
+      </div>
 
+      {/* <FontAwesomeIcon icon={faCheckSquare} size="lg" /> */}
+      <FontAwesomeIcon icon={["far", faHeart]} />
+
+      {/* <FontAwesomeIcon icon={faCar} size="lg" /> */}
+      {/* <FontAwesomeIcon icon="fa-light fa-heart" /> */}
       <Rating rating={props.rating} />
       {/* <p>{props.reviewsCount}</p> */}
-
       <p>{props.name}</p>
-      <p>{props.address}</p>
+      {/* <p>{props.address}</p> */}
       <p>{props.rating}</p>
       <p>{props.reviewsCount}</p>
       {/* <a href={props.urls}>{props.name}</a> */}
