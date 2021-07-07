fbcc80065222c16e435db458d0260f803ab47465
tandica <williams.tandica@gmail.com>
2021-07-07 18:14:51 -0400

    update styling to incorporate flex properly

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -2,55 +2,57 @@ import "../styles/UserProfile.scss";
 
 export default function UserProfile(props) {
   return (
-    <div className="user-profile-container">
+    <div>
       <h2>My Trips</h2>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
-      </div>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
-      </div>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
-      </div>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
-      </div>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
-      </div>
-      <div className="user-trip-card card">
-        <div className="city-name">Paris</div>
-        <img
-          className="user-trip-img"
-          alt="city"
-          src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
-        />
+      <div className="user-profile-container">
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
+        <div className="user-trip-card card">
+          <div className="city-name">Paris</div>
+          <img
+            className="user-trip-img"
+            alt="city"
+            src={`https://images.unsplash.com/photo-1539577514938-c7363678474c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60`}
+          />
+        </div>
       </div>
     </div>
   );
