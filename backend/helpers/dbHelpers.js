045c142866d2141bd74ebc423fdc12975cbc784f
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-07 18:27:24 -0400

    add query to database for city and check if they exist

--- backend/helpers/dbHelpers.js
+++ backend/helpers/dbHelpers.js
@@ -116,28 +116,14 @@ module.exports = (db) => {
       .catch((err) => err);
   };
 
-  //   const getUsersPosts = () => {
-  //     const query = {
-  //       text: `SELECT users.id as user_id, first_name, last_name, email, posts.id as post_id, title, content
-  //       FROM users
-  //       INNER JOIN posts
-  //       ON users.id = posts.user_id`,
-  //     };
-
-  //     return db
-  //       .query(query)
-  //       .then((result) => result.rows)
-  //       .catch((err) => err);
-  //   };
-
   const getCity = (name) => {
     const query = {
-      text: `SELECT name from cities WHERE name = ${name}`,
+      text: `SELECT * from cities WHERE short_name = ($1)`,
       values: [name],
     };
     return db
       .query(query)
-      .then((result) => result.rows[0])
+      .then((result) => (result.rows[0] ? result.rows[0] : null))
       .catch((err) => err);
   };
 
