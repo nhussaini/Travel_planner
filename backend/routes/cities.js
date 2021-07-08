a83620ddfc208768f7a92739c19aaddc7adf8911
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-07 23:45:53 -0400

    add codes for else condition when city doesnt exist, we fetch data for city and then redirect user to city page

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -29,142 +29,112 @@ module.exports = ({
     const googleCall = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+${cityName}&key=AIzaSyD6Gw9uN4YpFcH4cIjRbYbWKPl_vGQs0R0`;
 
     // console.log(findCity(cityName));
-    findCity(cityName).then((data) => {
-      if (data) {
-        console.log("Redirecting----");
+    findCity(cityName).then((city) => {
+      if (city) {
+        console.log("Reached IF---", cityName);
         res.redirect(`/cities/${cityName}`);
+      } else {
+        console.log("Reached ELSE---", cityName);
+        const allData = {};
+        axios
+          .get(
+            `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${cityName}`,
+            roadGoatApiAuth
+          )
+          .then((data) => {
+            const locationId = data.data.data[0].id;
+            // api call to get location data using the location id from previous call
+            axios
+              .get(
+                `https://api.roadgoat.com/api/v2/destinations/${locationId}`,
+                roadGoatApiAuth
+              )
+              .then((data) => {
+                allData.cityData = data.data.data;
+              });
+          });
+        // Getting images and googleData
+        Promise.all([
+          axios.get(imageCall),
+          // axios.get(weatherCall),
+          axios.get(googleCall),
+        ])
+          .then((all) => {
+            allData.imageData = all[0].data.results;
+            allData.googleData = all[1].data.results;
+            // console.log(all[0].data.results);
+
+            // extract the data coming from roadgoatApi and sav them in a variable
+            const {
+              short_name,
+              long_name,
+              population,
+              latitude,
+              longitude,
+              airbnb_url,
+              kayak_lodgings_url,
+              google_events_url,
+              alltrails_url,
+              getyourguide_url,
+              kayak_car_rental_url,
+            } = allData.cityData.attributes;
+
+            // add the city data to db
+            return addCity(
+              short_name,
+              long_name,
+              population,
+              latitude,
+              longitude,
+              airbnb_url,
+              kayak_lodgings_url,
+              google_events_url,
+              alltrails_url,
+              getyourguide_url,
+              kayak_car_rental_url
+            );
+            // res.send(allData);
+          })
+          .then((newCity) => {
+            // Save images to the image table
+            // console.log(allData.imageData);
+            for (let item of allData.imageData) {
+              addImage(item.urls.regular, newCity.id);
+            }
+
+            // save attractions for a place in the attraction table
+            for (let item of allData["googleData"]) {
+              if (item.user_ratings_total > 100) {
+                // saving the variable
+                const { name, formatted_address, rating, user_ratings_total } =
+                  item;
+                const { lat, lng } = item.geometry.location;
+                const photo_reference = item["photos"][0].photo_reference;
+                // saving each attraction in the db
+                addAttraction(
+                  name,
+                  formatted_address,
+                  lat,
+                  lng,
+                  rating,
+                  user_ratings_total,
+                  photo_reference,
+                  newCity.id
+                );
+              }
+            }
+          })
+          .then((data) => res.json(data))
+          .catch((err) =>
+            res.json({
+              error: err.message,
+            })
+          );
       }
     });
-    // getCity(cityName)
-    //   .then((city) => {
-    //     // if city doesnt exist in DB return error
-    //     if (city === null) {
-    //       return res.json("Sorry Cant find");
-    //     }
-    //     // if city exist, then grab all the data(details, images, attractions) for the city
-    //     const allData = {};
-    //     Promise.all([
-    //       getCity(cityName),
-    //       getImages(cityName),
-    //       getAttractions(cityName),
-    //     ])
-    //       .then((all) => {
-    //         allData.cityDetails = all[0];
-    //         allData.images = all[1];
-    //         allData.attractions = all[2];
-    //         res.json(allData);
-    //       })
-    //       .catch((err) => err);
-    //   })
-    //   .catch((err) => res.json(err));
-    // since Roaggoat data needs two api call, getting this data first before making calls to other api.
-    // first get the id of the location for further api calls
-    // axios
-    //   .get(
-    //     `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${cityName}`,
-    //     roadGoatApiAuth
-    //   )
-    //   .then((data) => {
-    //     const locationId = data.data.data[0].id;
-    //     // api call to get location data using the location id from previous call
-    //     axios
-    //       .get(
-    //         `https://api.roadgoat.com/api/v2/destinations/${locationId}`,
-    //         roadGoatApiAuth
-    //       )
-    //       .then((data) => {
-    //         allData.cityData = data.data.data;
-    //       });
-    //   });
-    // // Getting images and googleData
-    // Promise.all([
-    //   axios.get(imageCall),
-    //   // axios.get(weatherCall),
-    //   axios.get(googleCall),
-    // ])
-    //   .then((all) => {
-    //     allData.imageData = all[0].data.results;
-    //     allData.googleData = all[1].data.results;
-    //     // console.log(all[0].data.results);
-
-    //     // extract the data coming from roadgoatApi and sav them in a variable
-    //     const {
-    //       short_name,
-    //       long_name,
-    //       population,
-    //       latitude,
-    //       longitude,
-    //       airbnb_url,
-    //       kayak_lodgings_url,
-    //       google_events_url,
-    //       alltrails_url,
-    //       getyourguide_url,
-    //       kayak_car_rental_url,
-    //     } = allData.cityData.attributes;
-
-    //     // add the city data to db
-    //     return addCity(
-    //       short_name,
-    //       long_name,
-    //       population,
-    //       latitude,
-    //       longitude,
-    //       airbnb_url,
-    //       kayak_lodgings_url,
-    //       google_events_url,
-    //       alltrails_url,
-    //       getyourguide_url,
-    //       kayak_car_rental_url
-    //     );
-    //     // res.send(allData);
-    //   })
-    //   .then((newCity) => {
-    //     // Save images to the image table
-    //     // console.log(allData.imageData);
-    //     for (let item of allData.imageData) {
-    //       addImage(item.urls.regular, newCity.id);
-    //     }
-
-    //     // save attractions for a place in the attraction table
-    //     for (let item of allData["googleData"]) {
-    //       if (item.user_ratings_total > 100) {
-    //         // saving the variable
-    //         const { name, formatted_address, rating, user_ratings_total } =
-    //           item;
-    //         const { lat, lng } = item.geometry.location;
-    //         const photo_reference = item["photos"][0].photo_reference;
-    //         // saving each attraction in the db
-    //         addAttraction(
-    //           name,
-    //           formatted_address,
-    //           lat,
-    //           lng,
-    //           rating,
-    //           user_ratings_total,
-    //           photo_reference,
-    //           newCity.id
-    //         );
-    //       }
-    //     }
-    //   })
-    //   .then((data) => res.json(data))
-    //   .catch((err) =>
-    //     res.json({
-    //       error: err.message,
-    //     })
-    //   );
   });
 
-  router.post("/users", (req, res) => {
-    getUsers()
-      .then((users) => res.json(users))
-      .catch((err) =>
-        res.json({
-          error: err.message,
-        })
-      );
-  });
-
+  //Route for Individual City
   router.get("/:id", (req, res) => {
     const cityName = req.params.id;
     getCity(cityName).then((city) => {
@@ -188,17 +158,5 @@ module.exports = ({
         .catch((err) => err);
     });
   });
-  // eslint-disable-next-line func-style
-  // function getCityData(city) {
-  //   const allData = {};
-  //   Promise.all([getCity(city), getImages(city), getAttractions(city)])
-  //     .then((all) => {
-  //       allData.cityDetails = all[0];
-  //       allData.images = all[1];
-  //       allData.attractions = all[2];
-  //       allData;
-  //     })
-  //     .catch((err) => err);
-  // }
   return router;
 };
