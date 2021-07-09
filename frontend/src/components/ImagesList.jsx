7cb763f3f69c2cfe6d5f64dc4b0c43b414af28ca
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-09 01:31:18 -0400

    add state to hold carousel status

--- frontend/src/components/ImagesList.jsx
+++ frontend/src/components/ImagesList.jsx
@@ -2,11 +2,17 @@ import Image from "./Image";
 import "../styles/imageList.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { useState } from "react";
 
 export default function ImagesList(props) {
+  const [carouselStatus, setCarouselStatus] = useState(true);
   const allImages = props.images.map((image) => {
     return <Image key={image.id} alt={image.alt_description} src={image.url} />;
   });
+
+  function handleClick(e) {
+    setCarouselStatus(true);
+  }
   return (
     <div>
       <div>
@@ -14,22 +20,24 @@ export default function ImagesList(props) {
           {props.images.length ? `images of ${props.location}` : ""}
         </h2>
       </div>
-      <div className="image-container">{allImages}</div>
+      {/* <div className="image-container">{allImages}</div> */}
       <section className="w-50">
-        <Carousel>
-          <div>
-            <img src="https://images.unsplash.com/photo-1622495727422-badb2c5a688b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MXwxfHNlYXJjaHwxfHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
-            <p className="legend">Legend 1</p>
-          </div>
-          <div>
-            <img src="https://images.unsplash.com/photo-1581373449483-37449f962b6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MHwxfHNlYXJjaHw0fHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
-            <p className="legend">Legend 2</p>
-          </div>
-          <div>
-            <img src="https://images.unsplash.com/photo-1533658280853-e4a10c25a30d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MHwxfHNlYXJjaHw2fHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
-            <p className="legend">Legend 3</p>
-          </div>
-        </Carousel>
+        <button onClick={handleClick} className="btn btn-primary">
+          Click Me
+        </button>
+        {console.log(carouselStatus)}
+        {carouselStatus ? (
+          <Carousel autoPlay="true" width="25">
+            <div>
+              <img src="https://images.unsplash.com/photo-1622495727422-badb2c5a688b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MXwxfHNlYXJjaHwxfHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="https://images.unsplash.com/photo-1581373449483-37449f962b6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MHwxfHNlYXJjaHw0fHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
+              <p className="legend">Legend 2</p>
+            </div>
+          </Carousel>
+        ) : null}
       </section>
     </div>
   );
