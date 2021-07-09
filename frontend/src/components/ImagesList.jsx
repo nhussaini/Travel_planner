aa829c9ec709a8c730b409503171a417829af617
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-09 02:47:27 -0400

    clean up code

--- frontend/src/components/ImagesList.jsx
+++ frontend/src/components/ImagesList.jsx
@@ -6,22 +6,15 @@ import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 
 export default function ImagesList(props) {
-  const [carouselStatus, setCarouselStatus] = useState(false);
   const allImages = props.images.map((image) => {
     return <Image key={image.id} alt={image.alt_description} src={image.url} />;
   });
 
   const [show, setShow] = useState(false);
 
-  const handleClose = () => {
-    setShow(false);
-    // setCarouselStatus(true);
-  };
+  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  function handleClick(e) {
-    setCarouselStatus(true);
-  }
   return (
     <div>
       <div>
@@ -29,23 +22,8 @@ export default function ImagesList(props) {
           {props.images.length ? `images of ${props.location}` : ""}
         </h2>
       </div>
-      {/* <div className="image-container">{allImages}</div> */}
-      <section className="w-50">
-        <Button variant="danger">Primary</Button>
-        {console.log(carouselStatus)}
-        {/* {carouselStatus ? (
-          <Carousel autoPlay="true" width="25">
-            <div>
-              <img src="https://images.unsplash.com/photo-1622495727422-badb2c5a688b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MXwxfHNlYXJjaHwxfHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
-              <p className="legend">Legend 1</p>
-            </div>
-            <div>
-              <img src="https://images.unsplash.com/photo-1581373449483-37449f962b6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MHwxfHNlYXJjaHw0fHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
-              <p className="legend">Legend 2</p>
-            </div>
-          </Carousel>
-        ) : null} */}
-      </section>
+      <div className="image-container">{allImages}</div>
+      <section className="w-50"></section>
       <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button>
