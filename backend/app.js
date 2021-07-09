d06b8733a84011c7b8ae9997108db62f13208302
tandica <williams.tandica@gmail.com>
2021-07-09 15:19:30 -0400

    add conditional for if user is logged in

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-require('dotenv').config();
+require("dotenv").config();
 const db = require("./db");
 const dbHelpers = require("./helpers/dbHelpers")(db);
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/users", usersRouter(dbHelpers));
+app.use("/users", usersRouter(db));
 app.use("/userslogin", loginRouter(db));
 app.use("/cities", citiesRouter(dbHelpers));
 app.use("/", indexRouter);
