80a3c50354240099af930debfe02490fac8c79a4
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-09 02:39:06 -0400

    display modal

--- frontend/src/components/ImagesList.jsx
+++ frontend/src/components/ImagesList.jsx
@@ -3,12 +3,21 @@ import "../styles/imageList.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 export default function ImagesList(props) {
-  const [carouselStatus, setCarouselStatus] = useState(true);
+  const [carouselStatus, setCarouselStatus] = useState(false);
   const allImages = props.images.map((image) => {
     return <Image key={image.id} alt={image.alt_description} src={image.url} />;
   });
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+    // setCarouselStatus(true);
+  };
+  const handleShow = () => setShow(true);
 
   function handleClick(e) {
     setCarouselStatus(true);
@@ -22,11 +31,9 @@ export default function ImagesList(props) {
       </div>
       {/* <div className="image-container">{allImages}</div> */}
       <section className="w-50">
-        <button onClick={handleClick} className="btn btn-primary">
-          Click Me
-        </button>
+        <Button variant="danger">Primary</Button>
         {console.log(carouselStatus)}
-        {carouselStatus ? (
+        {/* {carouselStatus ? (
           <Carousel autoPlay="true" width="25">
             <div>
               <img src="https://images.unsplash.com/photo-1622495727422-badb2c5a688b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MXwxfHNlYXJjaHwxfHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
@@ -37,9 +44,29 @@ export default function ImagesList(props) {
               <p className="legend">Legend 2</p>
             </div>
           </Carousel>
-        ) : null}
+        ) : null} */}
       </section>
-      <section></section>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} dialogClassName="my-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel autoPlay="true" width="75">
+            <div>
+              <img src="https://images.unsplash.com/photo-1622495727422-badb2c5a688b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MXwxfHNlYXJjaHwxfHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
+              <p className="legend">Legend 1</p>
+            </div>
+            <div>
+              <img src="https://images.unsplash.com/photo-1581373449483-37449f962b6c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTMwNjh8MHwxfHNlYXJjaHw0fHxDaGljYWdvfGVufDB8MHx8fDE2MjU3NjYxNjk&ixlib=rb-1.2.1&q=80&w=1080" />
+              <p className="legend">Legend 2</p>
+            </div>
+          </Carousel>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
