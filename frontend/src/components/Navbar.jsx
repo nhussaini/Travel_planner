bbe60f65299529e87011056b4827a5834e7af418
tandica <williams.tandica@gmail.com>
2021-07-09 16:06:54 -0400

    render conditional navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,25 +2,45 @@ import "../styles/Navbar.scss";
 
 export default function Navbar() {
   const isLoggedIn = window.localStorage.getItem("user") ? true : false;
+  console.log(localStorage.getItem("user"));
 
-  return (
-    <header>
-      <div>
+  if (!localStorage.getItem("user")) {
+    return (
+      <header>
         <div>
-          <a href="/" className="nav-title">
-            TREK
-          </a>
+          <div>
+            <a href="/" className="nav-title">
+              TREK
+            </a>
+          </div>
+          <nav className="navbar-home" isLoggedIn={isLoggedIn}>
+            {/* <a href="/" className="login-nav">Logout</a> */}
+            <a href="/login" className="login-nav">
+              Login
+            </a>
+            <a href="/register" className="register-nav">
+              Register
+            </a>
+          </nav>
         </div>
-        <nav className="navbar-home" isLoggedIn={false}>
-          {/* <a href="/" className="login-nav">Logout</a> */}
-          <a href="/login" className="login-nav">
-            Login
-          </a>
-          <a href="/register" className="register-nav">
-            Register
-          </a>
-        </nav>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header>
+        <div>
+          <div>
+            <a href="/" className="nav-title">
+              TREK
+            </a>
+          </div>
+          <nav className="navbar-home" isLoggedIn={isLoggedIn}>
+            <a href="/" className="login-nav">
+              Logout
+            </a>
+          </nav>
+        </div>
+      </header>
+    );
+  }
 }
