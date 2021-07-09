93f8af6b2b9cdc1c3d4833da4429d9e890e64a33
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-09 18:53:17 -0400

    move map to citydetail component

--- frontend/src/components/CityDetail.jsx
+++ frontend/src/components/CityDetail.jsx
@@ -3,6 +3,7 @@ import { useCityData } from "../hooks/useCityData";
 import WeatherList from "./WeatherList";
 import ImagesList from "./ImagesList";
 import CityOverview from "./CityOverview";
+import GoogleMap from "./GoogleMap";
 import Navbar from "./Navbar";
 import ThingsToDoList from "./ThingsToDoList";
 import axios from "axios";
@@ -40,9 +41,11 @@ export default function CityDetail(props) {
       <Navbar />
       <CityLinkButtons locationData={state.locationData} />
       <ImagesList location={state.location} images={state.images} />
-      <CityOverview
-        locationData={state.locationData}
-        thingsToDo={state.thingsToDo}
+      <GoogleMap
+        lat={Number(state.locationData.latitude)}
+        lng={Number(state.locationData.longitude)}
+        location={state.locationData.long_name}
+        thingsToDo={state.thingsToDo.slice(0, 10)}
       />
       <ThingsToDoList location={state.location} thingsToDo={state.thingsToDo} />
       {state.location ? <WeatherList location={state.location} /> : null}
