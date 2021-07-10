3b902e982987dc3c1e598c7d6786a2573aa2c0a3
tandica <williams.tandica@gmail.com>
2021-07-10 15:10:06 -0400

    add new table to db

--- backend/db/schema/03_trip.sql
+++ backend/db/schema/03_trip.sql
@@ -1,19 +1,26 @@
 DROP TABLE IF EXISTS trip CASCADE;
 DROP TABLE IF EXISTS todo CASCADE;
+DROP TABLE IF EXISTS trip_attraction CASCADE;
 
 CREATE TABLE trip(
   id SERIAL PRIMARY KEY NOT NULL,
   date date NOT NULL,
   user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  city_id INT REFERENCES city(id) ON DELETE CASCADE
-  attraction_id INT REFERENCES attraction(id) ON DELETE CASCADE,
+  city_id INT REFERENCES city(id) ON DELETE CASCADE,
+  attraction_id INT REFERENCES attraction(id) ON DELETE CASCADE
 );
 
 CREATE TABLE todo(
   id SERIAL PRIMARY KEY NOT NULL,
   description VARCHAR(255)
   user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  trip_id INT REFERENCES trip(id) ON DELETE CASCADE
+)
+
+CREATE TABLE trip_attraction(
+  id SERIAL PRIMARY KEY NOT NULL,
   trip_id INT REFERENCES trip(id) ON DELETE CASCADE,
+  attraction_id INT REFERENCES attraction(id) ON DELETE CASCADE
 )
 
 
