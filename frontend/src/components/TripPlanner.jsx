642970b91962186bf5a735b6d55ebeae697044af
tandica <williams.tandica@gmail.com>
2021-07-10 16:53:53 -0400

    render attractions on new page

--- frontend/src/components/TripPlanner.jsx
+++ frontend/src/components/TripPlanner.jsx
@@ -1,7 +1,19 @@
 import { useLocation } from "react-router-dom";
+import ThingsToDoList from "./city/ThingsToDoList";
+import "styles/cityDetail.scss";
+//import "styles/TripPlanner.scss";
 
 export default function TripPlanner(props) {
   const location = useLocation();
-  console.log("statekey", location.state);
-  return <div>hello</div>;
+
+  const attractions = location.state.attractions;
+
+  console.log("attractions", attractions);
+  return (
+    <div className="map-attraction-container">
+      <section className="map-attraction">
+        <ThingsToDoList location={location.state.id} thingsToDo={attractions} />
+      </section>
+    </div>
+  );
 }
