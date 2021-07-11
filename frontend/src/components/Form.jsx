deeae8d0c07f4b34930f34eee8a54955d0b7a9f2
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-11 02:23:23 -0400

    add css for form container

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { Spinner, Button } from "react-bootstrap";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import "styles/home.scss";
 
 export default function Form(props) {
   let history = useHistory();
   const [input, setInput] = useState(null);
   const [error, setError] = useState(false);
+  const [loader, setLoader] = useState(false);
 
   function handleChange(e) {
     setInput(e.target.value);
@@ -26,31 +30,24 @@ export default function Form(props) {
       });
     }
   }
-
   return (
-    <div className="d-flex justify-content-center">
-      <form className="w-50" onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="inputLocation" className="form-label">
-            Choose a Destination
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputLocation"
-            aria-describedby="emailHelp"
-            placeholder="Choose a City"
-            onChange={handleChange}
-          />
-        </div>
-        {!input && error ? (
-          <div className="error-message">Please Type a City!</div>
-        ) : null}
-
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+    <form className="input-form" onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          className="form-control"
+          id="inputLocation"
+          aria-describedby="emailHelp"
+          placeholder="Choose a City"
+          onChange={handleChange}
+        />
+      </div>
+      {!input && error ? (
+        <div className="error-message">Please Type a City!</div>
+      ) : null}
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   );
 }
