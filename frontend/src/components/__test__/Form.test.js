9415fb98abf54e2fdc20b046f90182714755d1b1
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-13 18:53:06 -0400

    remove console log statements

--- frontend/src/components/__test__/Form.test.js
+++ frontend/src/components/__test__/Form.test.js
@@ -4,20 +4,12 @@ import {
   fireEvent,
   screen,
   prettyDOM,
+  findByText,
 } from "@testing-library/react";
 import Form from "components/Form";
 import Home from "components/Home";
 
 afterEach(cleanup);
-
-// const setup = () => {
-//   const form = render(<Form />);
-//   const input = form.getByLabelText("cost-input");
-//   return {
-//     input,
-//     ...form,
-//   };
-// };
 
 it("renders without crashing", () => {
   render(<Form />);
@@ -26,8 +18,29 @@ it("renders without crashing", () => {
 it("shows correct input", () => {
   const result = render(<Home />);
   const input = result.container.querySelector("#inputLocation");
-  console.log(prettyDOM(input));
-
   fireEvent.change(input, { target: { value: "London" } });
   expect((input, "London")).toBe("London");
 });
+
+// it("Submiting form without input shows Error", async () => {
+//   const result = render(<Home />);
+//   const button = result.getByText("Submit");
+//   fireEvent.click(button);
+//   const output = await screen.findByText("Please type a city name!");
+//   expect((output, "Please type a city name!")).toBe("Please type a city name!");
+// });
+
+// it.only("Shows Error if Cant find a matched City", async () => {
+//   const result = render(<Home />);
+//   const input = result.container.querySelector("#inputLocation");
+//   fireEvent.change(input, { target: { value: "ABCDEFGH" } });
+//   const button = result.getByText("Submit");
+//   fireEvent.click(button);
+//   const output = await screen.findByText(
+//     `Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`
+//   );
+//   expect(
+//     (output,
+//     `Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`)
+//   ).toBe(`Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`);
+// });
