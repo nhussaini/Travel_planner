7fca72d6a597c1cbba610314cb6748fe82d8e2fc
tandica <williams.tandica@gmail.com>
2021-07-12 20:47:45 -0400

    implement logout functionality which clears local storage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,14 @@ import { Navbar, Nav, Button } from "react-bootstrap";
 export default function NavBar() {
   console.log("current user******", localStorage.getItem("user"));
   const isLoggedIn = window.localStorage.getItem("user") ? true : false;
+  //const logout = window.localStorage.removeItem("user");
+
+  function logout() {
+    localStorage.clear();
+  }
 
   if (!localStorage.getItem("user")) {
+    localStorage.clear();
     return (
       <>
         <Navbar bg="dark" variant="dark" className="nav-container" sticky="top">
@@ -24,19 +30,18 @@ export default function NavBar() {
       </>
     );
   } else {
-    //localStorage.clear();
-    sessionStorage.clear();
+    //sessionStorage.clear();
     return (
       <>
         <Navbar bg="dark" variant="dark" className="nav-container" sticky="top">
           <Navbar.Brand href="/" className="nav-title">
             <span>TREK</span>
           </Navbar.Brand>
-          <Nav className="mr-auto navbar-links" isLoggedIn={isLoggedIn}>
+          <Nav className="mr-auto navbar-links">
             <Button href="/login" className="btn btn-light">
               Your Profile
             </Button>
-            <Button href="/login" className="btn btn-light">
+            <Button href="/login" className="btn btn-light" onClick={logout}>
               Logout
             </Button>
           </Nav>
