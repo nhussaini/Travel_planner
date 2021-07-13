851a047528952233816712161ec1b215ef4f6e1d
tandica <williams.tandica@gmail.com>
2021-07-13 16:28:37 -0400

    update your profile button to link to user profile page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import "styles/nav.scss";
 import { Navbar, Nav, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 export default function NavBar() {
+  let history = useHistory();
+  //console.log("USER ID >>>>>>", localStorage.getItem("user"));
+  //const getUserID = localStorage.getItem("user");
+  //console.log(getUserID.id);
+  // const id = JSON.stringify(getUserID);
+  // //console.log("ID >>>", id);
+
+  //function getUserID(user) {
+  // const obj = JSON.parse(localStorage.getItem("user"));
+  // console.log("json id>>>>>>", obj.id);
+  // const userProfile = `/users/${obj.id}`;
+  // console.log("user prof", userProfile);
+  // }
+
+  function handleClick(e) {
+    e.preventDefault();
+    const obj = JSON.parse(localStorage.getItem("user"));
+    const id = obj.id;
+    history.push(`/users/${id}`);
+  }
+
   function logout() {
     localStorage.clear();
   }
@@ -33,7 +56,7 @@ export default function NavBar() {
             <span>TREK</span>
           </Navbar.Brand>
           <Nav className="mr-auto navbar-links">
-            <Button href="/login" className="btn btn-light">
+            <Button className="btn btn-light" onClick={handleClick}>
               Your Profile
             </Button>
             <Button
