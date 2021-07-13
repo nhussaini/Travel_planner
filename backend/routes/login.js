7e028112eeb694fa85951f327554c0c6ebf9a941
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-13 00:29:18 -0400

    shoe error if email doesnt exist

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -13,19 +13,14 @@ module.exports = (db) => {
 
     db.query(queryString, [email]).then((data) => {
       if (data.rows.length === 0) {
-        return res
-          .status(400)
-          .send({ message: "User not found", status: "error" });
+        console.log("Do not Exist");
+        return res.send(null);
       }
-
       const user = data.rows[0];
       if (user.password !== password) {
         return res.send(null);
       }
-      console.log("Before---", user);
       delete user.password;
-      console.log("its a Match!!!!");
-      console.log(user);
       return res.status(200).send(user);
     });
   });
