056b026cb5028f158affc9cff7ca1218ff27a033
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-13 19:14:02 -0400

    add tests to check form validation

--- frontend/src/components/__test__/Form.test.js
+++ frontend/src/components/__test__/Form.test.js
@@ -22,25 +22,19 @@ it("shows correct input", () => {
   expect((input, "London")).toBe("London");
 });
 
-// it("Submiting form without input shows Error", async () => {
-//   const result = render(<Home />);
-//   const button = result.getByText("Submit");
-//   fireEvent.click(button);
-//   const output = await screen.findByText("Please type a city name!");
-//   expect((output, "Please type a city name!")).toBe("Please type a city name!");
-// });
+it("Submiting form without input shows Error", async () => {
+  const result = render(<Home />);
+  const button = result.getByText("Submit");
+  fireEvent.click(button);
+  const output = await screen.findByText("Please type a city name!");
+  expect((output, "Please type a city name!")).toBe("Please type a city name!");
+});
 
-// it.only("Shows Error if Cant find a matched City", async () => {
-//   const result = render(<Home />);
-//   const input = result.container.querySelector("#inputLocation");
-//   fireEvent.change(input, { target: { value: "ABCDEFGH" } });
-//   const button = result.getByText("Submit");
-//   fireEvent.click(button);
-//   const output = await screen.findByText(
-//     `Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`
-//   );
-//   expect(
-//     (output,
-//     `Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`)
-//   ).toBe(`Sorry, we couldn't find "ABCDEFGH" worldwide, try a different city.`);
-// });
+it("Shows Error if Cant find a matched City", () => {
+  const result = render(<Home />);
+  const input = result.container.querySelector("#inputLocation");
+  fireEvent.change(input, { target: { value: "Toronto" } });
+  const button = result.getByText("Submit");
+  fireEvent.click(button);
+  expect(result.getByText("Loading.....")).toBeInTheDocument();
+});
