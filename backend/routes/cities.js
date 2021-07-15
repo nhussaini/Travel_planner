221d5c8ec11ce30e511b43df5e80d293581a1172
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-15 11:54:34 -0400

    update API Keys

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -17,17 +17,17 @@ module.exports = ({
   //roadGhoat Credentials
   const roadGoatApiAuth = {
     auth: {
-      username: "aee3dc619b9da58ca3a967b6cb7a4fc5",
-      password: "01f998155debd93205287912664cb75c",
+      username: process.env.ROADGOATUSER,
+      password: process.env.ROADGOATSECRET,
     },
   };
 
   router.post("/getCityData", async (req, res) => {
     const cityName = req.body.userInput;
     // saving all api quesries in variables
-    const imageCall = `https://api.unsplash.com/search/photos?page=1&query=${cityName}&client_id=${process.env.imageKEY}&per_page=10&orientation=landscape`;
+    const imageCall = `https://api.unsplash.com/search/photos?page=1&query=${cityName}&client_id=${process.env.IMAGEKEY}&per_page=10&orientation=landscape`;
     // const weatherCall = `https://api.weatherbit.io/v2.0/forecast/daily?city=${req.body.userInput}&key=${process.env.weatherKEY}&days=7`;
-    const googleCall = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+${cityName}&key=AIzaSyD6Gw9uN4YpFcH4cIjRbYbWKPl_vGQs0R0`;
+    const googleCall = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=attractions+${cityName}&key=${process.env.GOOGLEKEY}`;
 
     // console.log(findCity(cityName));
     const matchedCity = await findCity(cityName);
